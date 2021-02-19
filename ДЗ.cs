6da67844a0838c17766bdc7e4280b875a5879ea9
Gikbrains <79279039+Gikbrains@users.noreply.github.com>
2021-02-19 10:45:27 +0300

    Add files via upload

---
+++ ДЗ.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ваше имя: ");
+            Console.WriteLine($"Привет, {Console.ReadLine()}, сегодня {DateTime.Today.ToLongDateString()}");
+            Console.WriteLine("Нажмите на любую кнопку для выхода из программы.");
+            Console.ReadKey();
+        }
+}
